7108b60138391e525207e74928ab8d743f8f1347
leo <leo.kalshteyn@gmail.com>
2019-12-11 20:39:11 -0800

    added redux store and reducers to index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+
+import teamsReducer from './reducers/TeamsReducer.js';
+import playersReducer from './reducers/PlayersReducer.js';
+import starplayersReducer from './reducers/StarPlayersReducer.js';
+
+const reducer = combineReducers({
+  teams:teamsReducer,
+  players:playersReducer,
+  star_players:starplayersReducer
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
